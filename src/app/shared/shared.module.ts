490ebe1c3704402cfca19773f61376710e48ee21
nicholassinggih <nicholas.singgih@#gmail.com>
2023-05-25 10:47:55 +0700

    chore(lib):install primeng

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { PropertyService } from './services/property.service';
-
-
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule
   ],
   providers: [
     PropertyService
+  ],
+  exports: [
+    FormsModule
   ]
 })
 export class SharedModule { }
