fca404aba1d7d2c14ea8a6130f1d52603ac25912
nicholassinggih <nicholas.singgih@#gmail.com>
2023-06-04 21:24:24 +0700

    feat(icons):add icon buttons
    -add primeicons and fortawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleInfo, faEdit, faFile, fas } from '@fortawesome/free-solid-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { HomeComponent } from './home/home.component';
+import { MetaComponent } from './meta/meta/meta.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SharedModule } from './shared/shared.module';
-import { MetaComponent } from './meta/meta/meta.component';
 
 @NgModule({
   declarations: [
@@ -24,9 +26,17 @@ import { MetaComponent } from './meta/meta/meta.component';
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+    library.addIcons(faCircleInfo);
+    library.addIcons(faFile);
+    library.addIcons(faEdit);
+  }
+}
