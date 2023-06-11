ec7d57bca2919bd04cf364e7076d9048787e12f1
nicholassinggih <nicholas.singgih@#gmail.com>
2023-06-11 22:32:16 +0700

    feat(prop-list):view button and detail page
    -when clicked, user is redirected to property detail page

--- src/app/property/property.module.ts
+++ src/app/property/property.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { PropertyRoutingModule } from './property-routing.module';
 
@@ -8,7 +9,7 @@ import { PropertyRoutingModule } from './property-routing.module';
 
 @NgModule({
   declarations: [
-    // PropertyDetailComponent,
+    PropertyDetailComponent,
     PropertyListComponent
   ],
   imports: [
@@ -17,7 +18,7 @@ import { PropertyRoutingModule } from './property-routing.module';
     SharedModule
   ],
   exports: [
-    // PropertyDetailComponent
+    PropertyDetailComponent
   ]
 })
 export class PropertyModule { }
