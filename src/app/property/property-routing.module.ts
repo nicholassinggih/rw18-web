f215e5f835ee870693cb29b4273c56102ed20bea
nicholassinggih <nicholas.singgih@#gmail.com>
2023-06-11 21:59:12 +0700

    feat(prop-detail):add property detail component

--- src/app/property/property-routing.module.ts
+++ src/app/property/property-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContainerComponent } from '../shared/container/container.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 
 const routes: Routes = [
@@ -8,7 +9,9 @@ const routes: Routes = [
     path:'',
     component: ContainerComponent,
     children: [
-      { path:'', component: PropertyListComponent}
+      { path:'', component: PropertyListComponent},
+      { path:':id', component: PropertyDetailComponent}
+
     ]
   }
 ];
