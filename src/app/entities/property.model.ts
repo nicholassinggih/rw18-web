ac56adc71567b7f753aee8a5653d546a65c237ed
nicholassinggih <nicholas.singgih@#gmail.com>
2023-06-29 22:12:51 +0700

    feat(property):add new model

---
+++ src/app/entities/property.model.ts
@@ -0,0 +1,10 @@
+export class Property {
+  id: number = 0;
+  blok: string = '';
+  no: string = '';
+  nominal: number = 0;
+  komersial: boolean = false;
+  rt: number = 0;
+  rw: number = 0;
+
+}
