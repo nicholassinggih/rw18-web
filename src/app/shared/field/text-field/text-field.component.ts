21d33f95fee96015f70768ad94f83bb36f3e47f9
nicholassinggih <nicholas.singgih@#gmail.com>
2023-06-29 22:05:39 +0700

    feat(text-field):new text field component

---
+++ src/app/shared/field/text-field/text-field.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-text-field',
+  templateUrl: './text-field.component.html',
+  styleUrls: ['./text-field.component.scss']
+})
+export class TextFieldComponent {
+  @Input() title: string;
+  value: string;
+}
