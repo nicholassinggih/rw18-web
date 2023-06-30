46a727f4899afedbf551ce0652c58355725f3ad5
nicholassinggih <nicholas.singgih@#gmail.com>
2023-06-30 12:39:11 +0700

    feat(prop-detail):display basic attributes
    -a page that displays the details of a single property

--- src/app/property/property-detail/property-detail.component.ts
+++ src/app/property/property-detail/property-detail.component.ts
@@ -11,6 +11,62 @@ export class PropertyDetailComponent implements OnInit {
   propertyId: string | null = null;
   detail: any;
 
+  get pemilik(): string{
+    return this.detail?.Pemilik?.nama;
+  }
+
+  set pemilik(value: string) {
+    if (!this.detail?.Pemilik) return;
+    this.detail.Pemilik.nama = value;
+  }
+
+  get blok(): string {
+    return this.detail?.blok;
+  }
+
+  set blok(value: string) {
+    if (!this.detail) return;
+    this.detail.blok = value;
+  }
+
+  get no(): string {
+    return this.detail?.no;
+  }
+
+  set no(value: string) {
+    if (!this.detail) return;
+    this.detail.no = value;
+  }
+
+  get rt(): string {
+    return this.detail?.rt;
+  }
+
+  set rt(value: string) {
+    if (!this.detail) return;
+    this.detail.rt = value;
+  }
+
+  get collector(): string {
+    return this.detail?.Collector?.nama;
+  }
+
+  set collector(value: string) {
+    if (!this.detail?.Collector) return;
+    this.detail.Collector.nama = value;
+  }
+
+  get commercial(): string {
+    return this.detail?.komersial;
+  }
+
+  set commercial(value: string) {
+    if (!this.detail?.komersial) return;
+    this.detail.komersial.nama = value;
+  }
+
+
+
   constructor(
     private propertyService: PropertyService,
     private activatedRoute: ActivatedRoute
--- src/app/shared/field/text-field/text-field.component.ts
+++ src/app/shared/field/text-field/text-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-text-field',
@@ -7,5 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class TextFieldComponent {
   @Input() title: string;
-  value: string;
+  @Input() value: any;
+  @Output() valueChange: EventEmitter<any> = new EventEmitter<any>();
 }
