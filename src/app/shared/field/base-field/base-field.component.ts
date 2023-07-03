f47732f838534dc2bd6ecc30a6bfe0e2193089e7
nicholassinggih <nicholas.singgih@#gmail.com>
2023-07-03 18:17:40 +0700

    feat(base-field):watch value change
    -add call to valueChange.emit and another listener 'onChange'

--- src/app/shared/field/base-field/base-field.component.ts
+++ src/app/shared/field/base-field/base-field.component.ts
@@ -9,6 +9,19 @@ export class BaseFieldComponent {
 
   @Input() disabled: boolean = false;
   @Input() title: string;
-  @Input() value: any;
+
+  _value: any;
+  @Input()
+  get value():any {
+    return this._value;
+  }
+  set value(val: any) {
+    this._value = val;
+    this.valueChange.emit(val);
+    this.onChange.emit(val);
+  }
+
   @Output() valueChange: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
+
 }
