e0fe3c4742a05c9de9c5b873ab129014d4ae73f3
nicholassinggih <nicholas.singgih@#gmail.com>
2023-08-16 22:07:45 +0700

    feat(property-list):new button to bills page

--- src/app/property/property.module.ts
+++ src/app/property/property.module.ts
@@ -4,13 +4,15 @@ import { SharedModule } from '../shared/shared.module';
 import { PropertyDetailComponent } from './property-detail/property-detail.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { PropertyRoutingModule } from './property-routing.module';
+import { PropertyBillComponent } from './property-bill/property-bill.component';
 
 
 
 @NgModule({
   declarations: [
     PropertyDetailComponent,
-    PropertyListComponent
+    PropertyListComponent,
+    PropertyBillComponent
   ],
   imports: [
     PropertyRoutingModule,
