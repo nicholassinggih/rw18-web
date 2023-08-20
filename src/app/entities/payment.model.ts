9d9e6c669820fff9c94a3b03b383c628c1c47850
nicholassinggih <nicholas.singgih@#gmail.com>
2023-08-20 16:35:36 +0700

    feat(payment):add payment page
    -component for page and the new model 'payment'

---
+++ src/app/entities/payment.model.ts
@@ -0,0 +1,11 @@
+
+export class Property {
+  id: number = 0;
+  amount: number;
+  accountId: number;
+  fromName: string;
+  fromAccountNo: string;
+  fromBank: string;
+  notes: string;
+  phonetic: string;
+}
