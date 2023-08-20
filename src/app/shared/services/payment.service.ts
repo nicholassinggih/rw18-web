205bfabec14e7ac5e2510d5c402ed9e3930a3edb
nicholassinggih <nicholas.singgih@#gmail.com>
2023-08-20 18:53:16 +0700

    feat(add-payment):call to api and toast message

--- src/app/shared/services/payment.service.ts
+++ src/app/shared/services/payment.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Payment } from 'src/app/entities/payment.model';
 import { environment } from 'src/environments/environment';
 
@@ -12,7 +13,7 @@ export class PaymentService {
     private http: HttpClient
   ) { }
 
-  savePayment(payment: Payment) {
+  savePayment(payment: Payment): Observable<any> {
     let url = `${environment.apiUrl}/payment/add`;
     return this.http.post<any>(url, payment);
   }
